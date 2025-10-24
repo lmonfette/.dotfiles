0ba5ae5322586f51223a0b0101f0b49728bdb86a
lmonfette <louis-alexandre.monfette@sparkmicro.com>
2025-10-24 17:36:57 -0400

    re-organize each tools and added stow scripts

---
+++ stow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# setup alacritty
+stow alacritty
+
+# setup neovim
+stow nvim
+
+# setup sway
+stow sway
+
+# setup tmux
+stow tmux
+
+# setup zsh
+stow zsh
---
+++ unstow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# setup alacritty
+stow -D alacritty
+
+# setup neovim
+stow -D nvim
+
+# setup sway
+stow -D sway
+
+# setup tmux
+stow -D tmux
+
+# setup zsh
+stow -D zsh
